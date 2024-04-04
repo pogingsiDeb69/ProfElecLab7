10a8b73ebaf7cb2bfbf5366690cf7c666f9b6e5e
pogingsiDeb79 <alexantopina@gmail.com>
2024-04-04 22:51:25 +0800

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',function(){
     const child = document.getElementsByTagName("span");
 const parent = child.parentNode;
-console.log(parent);
+console.log(parent.nodeName);
 });
